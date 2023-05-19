714555124e4e456b79836dd822095a5871ccf48a
Undhad Kaushik <kpkushpatel8@gmail.com>
2023-05-19 07:20:46 +0530

    Initial Commit

---
+++ ListViewSwiftUI/ListViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewSwiftUIApp.swift
+//  ListViewSwiftUI
+//
+//  Created by undhad kaushik on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ListViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
